49a4657b530ff1654fc1e8701cfd713901f4ce8e
ErickW01 <36089754+ErickW01@users.noreply.github.com>
2024-08-18 16:59:21 -0700

    profile img tweak

--- app/components/ProfilePic.tsx
+++ app/components/ProfilePic.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import moi from '../../public/imgs/Moi.jpg'
 
 export default function ProfilePic() {
   return (
     <section className='w-full mx-auto'>
         <Image className='border-4 border-black dark:border-slate-500
         drop-shadow-xl shadow-black rounded-full mx-auto mt-8'
-        src={"/imgs/moi.jpg"}
+        src={moi}
         width={200}
         height={200}
         alt="Erick W."
